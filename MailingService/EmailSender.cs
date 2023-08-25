5605765d614cb09320690750aa0b7256af5e446d
Kristian <kristiannrg@gmail.com>
2023-08-25 19:02:34 +0200

    Email Sender

--- MailingService/EmailSender.cs
+++ MailingService/EmailSender.cs
@@ -7,7 +7,11 @@ namespace MailingService
 {
 	public class EmailSender : IEmailSender
 	{
-		public EmailSender()
+
+        private readonly string _outlookMail = Environment.GetEnvironmentVariable("OUTLOOK_MAIL") ?? "travelo-reservations@outlook.com";
+        private readonly string _outlookPass = Environment.GetEnvironmentVariable("OUTLOOK_PASS") ?? "Travelo-pass123";
+
+        public EmailSender()
 		{
 		}
 
@@ -17,11 +21,11 @@ namespace MailingService
             {
                 EnableSsl = true,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("travelo-reservations@outlook.com", "Travelo-pass123")
+                Credentials = new NetworkCredential(_outlookMail, _outlookPass)
             };
 
             return client.SendMailAsync(
-                new MailMessage(from: "travelo-reservations@outlook.com",
+                new MailMessage(from: _outlookMail,
                                 to: email,
                                 subject,
                                 message
